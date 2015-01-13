3cbc8a03c7e2ca5fd93c368484d46f2244728796
gongiskhan <goncalo.p.gomes@googlemail.com>
2015-01-13 14:11:12 +0000

    Update ImmobilizePlugin.h

--- src/ios/ImmobilizePlugin.h
+++ src/ios/ImmobilizePlugin.h
@@ -1,29 +1,19 @@
 //
-//  CDVBackgroundGeoLocation.h
-//
-//  Created by Chris Scott <chris@transistorsoft.com>
+//  Immobilize.h
 //
 
 #import <Cordova/CDVPlugin.h>
 #import "CDVLocation.h"
-#import <AudioToolbox/AudioToolbox.h>
 
-@interface CDVBackgroundGeoLocation : CDVPlugin <CLLocationManagerDelegate>
+@interface Immobilize : CDVPlugin <CLLocationManagerDelegate>
 
 @property (nonatomic, strong) NSString* syncCallbackId;
-@property (nonatomic, strong) NSMutableArray* stationaryRegionListeners;
 
-- (void) configure:(CDVInvokedUrlCommand*)command;
-- (void) start:(CDVInvokedUrlCommand*)command;
-- (void) stop:(CDVInvokedUrlCommand*)command;
+- (void) update:(CDVInvokedUrlCommand*)command;
+- (void) stopUpdate:(CDVInvokedUrlCommand*)command;
 - (void) finish:(CDVInvokedUrlCommand*)command;
 - (void) onPaceChange:(CDVInvokedUrlCommand*)command;
-- (void) setConfig:(CDVInvokedUrlCommand*)command;
-- (void) addStationaryRegionListener:(CDVInvokedUrlCommand*)command;
-- (void) getStationaryLocation:(CDVInvokedUrlCommand *)command;
-- (void) onSuspend:(NSNotification *)notification;
 - (void) onResume:(NSNotification *)notification;
 - (void) onAppTerminate;
 
 @end
-
